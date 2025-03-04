becf6662f5563ac83223aa84d625863c7817de26
David Culemann <davidculemann@gmail.com>
2025-03-04 21:28:31 +0000

    fix cv editor layout

--- app/routes/_app._documents.cvs.$id.tsx
+++ app/routes/_app._documents.cvs.$id.tsx
@@ -8,24 +8,17 @@ export default function CV() {
 		<ResizablePanelGroup direction="horizontal" className="border h-full">
 			<ResizablePanel>
 				<div className="flex items-center justify-center p-6">
-					<span className="font-semibold">One</span>
+					<span className="font-semibold">Editor</span>
 				</div>
 			</ResizablePanel>
 			<ResizableHandle />
 			<ResizablePanel>
-				<ResizablePanelGroup direction="vertical">
-					<ResizablePanel>
-						<div className="flex h-full items-center justify-center p-6">
-							<span className="font-semibold">Two</span>
-						</div>
-					</ResizablePanel>
-					<ResizableHandle />
-					<ResizablePanel>
-						<div className="flex h-full items-center justify-center p-6">
-							<span className="font-semibold">Three</span>
-						</div>
-					</ResizablePanel>
-				</ResizablePanelGroup>
+				<ResizablePanel>
+					<div className="flex h-full items-center justify-center p-6">
+						<span className="font-semibold">Preview</span>
+					</div>
+				</ResizablePanel>
+				<ResizableHandle />
 			</ResizablePanel>
 		</ResizablePanelGroup>
 	);

03dce650c7e00807c29a6b52bd0ccf02aeef05d4
David Culemann <davidculemann@gmail.com>
2025-03-04 22:35:12 +0000

    wrap page button with motion.div

--- app/components/shared/page-button.tsx
+++ app/components/shared/page-button.tsx
@@ -1,9 +1,13 @@
+import { containerVariants } from "@/lib/framer/animations";
+import { motion } from "motion/react";
 import { Button } from "../ui/button";
 
 export default function PageButton({ children, onClick }: { children: React.ReactNode; onClick: () => void }) {
 	return (
-		<Button onClick={onClick} variant="outline" className="h-64 w-48">
-			{children}
-		</Button>
+		<motion.div layout {...containerVariants}>
+			<Button onClick={onClick} variant="outline" className="h-64 w-48">
+				{children}
+			</Button>
+		</motion.div>
 	);
 }

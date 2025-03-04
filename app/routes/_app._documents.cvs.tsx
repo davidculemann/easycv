e293a0812a89d8791082be09c0176800e4b33ffb
David Culemann <davidculemann@gmail.com>
2025-03-04 20:40:58 +0000

    use pagebutton for documents

--- app/routes/_app._documents.cvs.tsx
+++ app/routes/_app._documents.cvs.tsx
@@ -1,32 +1,27 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import PageButton from "@/components/shared/page-button";
 import { useCV } from "@/hooks/api-hooks/useCV";
-import { FilePlus } from "lucide-react";
+import { useNavigate } from "@remix-run/react";
 
 export default function CVs() {
 	const { cvs } = useCV();
-
+	const navigate = useNavigate();
 	const { createCV } = useCV();
 
 	const handleCreateCV = () => {
 		createCV();
 	};
 
+	const handleOpenCV = (id: string) => {
+		navigate(`/cvs/${id}`);
+	};
+
 	return (
 		<div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-			<Card className="flex flex-col items-center justify-center w-48 h-48" onClick={handleCreateCV}>
-				<CardHeader>
-					<CardTitle>Create New CV</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<FilePlus className="w-18 h-18 " />
-				</CardContent>
-			</Card>
+			<PageButton onClick={handleCreateCV}>Create New CV</PageButton>
 			{cvs?.map((cv: any) => (
-				<Card key={cv.id}>
-					<CardHeader>
-						<CardTitle>{cv.title}</CardTitle>
-					</CardHeader>
-				</Card>
+				<PageButton key={cv.id} onClick={() => handleOpenCV(cv.id)}>
+					{cv.title}
+				</PageButton>
 			))}
 		</div>
 	);

ea65112f5f1b5b7b244f39d5adc2e02c28d33461
David Culemann <davidculemann@gmail.com>
2025-03-15 17:30:32 +0000

    opt in to future flags

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from "vite";
 import envOnly from "vite-env-only";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/node" {
+	interface Future {
+		v3_singleFetch: true;
+	}
+}
+
 export default defineConfig({
 	plugins: [
 		envOnly(),
@@ -14,6 +20,8 @@ export default defineConfig({
 				v3_fetcherPersist: true,
 				v3_relativeSplatPath: true,
 				v3_throwAbortReason: true,
+				v3_singleFetch: true,
+				v3_lazyRouteDiscovery: true,
 			},
 		}),
 	],

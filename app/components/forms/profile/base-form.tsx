f79804abc45b8d84102753ddde81bb1810518e73
David Culemann <davidculemann@gmail.com>
2025-05-04 19:18:32 +0400

    allow base-form to take an onsubmit or method

--- app/components/forms/profile/base-form.tsx
+++ app/components/forms/profile/base-form.tsx
@@ -1,28 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { CardContent, CardFooter } from "@/components/ui/card";
 import { Form } from "@/components/ui/form";
+import { type FormMethod, Form as RemixForm } from "@remix-run/react";
 import type { UseFormReturn } from "react-hook-form";
 
 export interface BaseFormProps {
 	form: UseFormReturn<any>;
-	onSubmit: (data: any) => void;
+	onSubmit?: (data: any) => void;
+	method?: FormMethod;
 	isSubmitting?: boolean;
 	children: React.ReactNode;
 }
 
-export function BaseForm({ form, onSubmit, isSubmitting, children }: BaseFormProps) {
+export function BaseForm({ form, onSubmit, method, isSubmitting, children }: BaseFormProps) {
 	const canSubmit = form.formState.isValid && form.formState.isDirty;
 
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex-1">
+			<RemixForm {...{ onSubmit, method }} className="space-y-8 flex-1">
 				<CardContent>{children}</CardContent>
 				<CardFooter>
 					<Button type="submit" disabled={!canSubmit || isSubmitting} className="w-full">
 						{isSubmitting ? "Saving..." : "Save Changes"}
 					</Button>
 				</CardFooter>
-			</form>
+			</RemixForm>
 		</Form>
 	);
 }

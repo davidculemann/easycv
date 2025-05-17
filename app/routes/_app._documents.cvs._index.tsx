1197a4a24a2bc1834ee34d0cf8db69d406b084b9
David Culemann <davidculemann@gmail.com>
2025-05-17 13:49:06 +0100

    improved functionality on cvs page

--- app/routes/_app._documents.cvs._index.tsx
+++ app/routes/_app._documents.cvs._index.tsx
@@ -1,6 +1,8 @@
-import PageButton from "@/components/shared/page-button";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { useCV } from "@/hooks/api-hooks/useCV";
 import { formatDate } from "@/lib/dates";
+import { tileEntryExit } from "@/lib/framer/animations";
 import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
 import { createCVDocument, getCVDocuments } from "@/lib/supabase/documents/cvs";
 import type { SupabaseOutletContext } from "@/lib/supabase/supabase";
@@ -9,8 +11,7 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData, useNavigate, useOutletContext } from "@remix-run/react";
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
-import { CirclePlus } from "lucide-react";
-import { AnimatePresence } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { toast } from "sonner";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -31,9 +32,9 @@ function CVList() {
 
 	const navigate = useNavigate();
 
-	async function handleCreateNewDocument() {
+	async function handleCreateNewDocument(fromProfile?: boolean) {
 		try {
-			const newDocument = await createCVDocument({ supabase, cvName: "New CV", fromProfile: true });
+			const newDocument = await createCVDocument({ supabase, cvName: "New CV", fromProfile });
 			if ("id" in newDocument && newDocument.id) {
 				navigate(`/cvs/${newDocument.id}`);
 			} else throw new Error("No document ID returned");
@@ -48,25 +49,31 @@ function CVList() {
 
 	return (
 		<AnimatePresence>
-			<PageButton type="submit" onClick={handleCreateNewDocument} newDocument>
-				<span className="flex flex-col justify-center items-center gap-2">
-					Create New CV
-					<CirclePlus size={48} />
-				</span>
-			</PageButton>
+			<Card className="h-52 w-40 sm:h-64 sm:w-48 border-dashed border-muted-foreground border flex flex-col items-center justify-center gap-3">
+				<div className="font-semibold text-center">Create New CV</div>
+				<div className="flex flex-col gap-2 w-full px-2">
+					<Button variant="outline" className="w-full" onClick={() => handleCreateNewDocument(true)}>
+						From profile
+					</Button>
+					<Button variant="outline" className="w-full" onClick={() => handleCreateNewDocument(false)}>
+						From scratch
+					</Button>
+				</div>
+			</Card>
 
 			{cvs?.data?.map((cv: any) => (
-				<PageButton
-					animate
-					key={cv.id}
-					onClick={() => handleOpenCV(cv.id)}
-					style={{ viewTransitionName: `cv-card-${cv.id}` }}
-				>
-					<span className="flex flex-col justify-center items-center gap-2">
-						<span className="text-lg font-bold">{cv.title}</span>
-						<span className="text-sm text-muted-foreground">{formatDate(cv.created_at)}</span>
-					</span>
-				</PageButton>
+				<motion.div key={cv.id} {...tileEntryExit} style={{ viewTransitionName: `cv-card-${cv.id}` }}>
+					<Button
+						onClick={() => handleOpenCV(cv.id)}
+						variant="outline"
+						className={"h-52 w-40 sm:h-64 sm:w-48"}
+					>
+						<span className="flex flex-col justify-center items-center gap-2">
+							<span className="text-lg font-bold">{cv.title}</span>
+							<span className="text-sm text-muted-foreground">{formatDate(cv.created_at)}</span>
+						</span>
+					</Button>
+				</motion.div>
 			))}
 		</AnimatePresence>
 	);
